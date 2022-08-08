83f4229e89171d1ef365bc3b1fd22a0766e8a09b
Flash1714 <shaurya1714@gmail.com>
2022-08-08 12:17:43 +0530

    Add project files.

---
+++ CalculatorProject/Form1.cs
@@ -0,0 +1,15 @@
+namespace CalculatorProject
+{
+    public partial class CalculatorProject : Form
+    {
+        public CalculatorProject()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculatorProject_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
